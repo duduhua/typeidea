fc75762391abe61eb6bc14817b480ce9bf605438
duduhua <1980271804@qq.com>
2019-10-30 14:24:56 +0800

    第三方插件，调试调优，mysql

--- typeidea/comment/urlcomment.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import CommentView
-
-urlpatterns = [
-    path('', CommentView.as_view(), name='comment'),
-]
--- typeidea/config/urlsconfig.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from .views import LinkListView
-
-urlpatterns = [
-    path('', LinkListView.as_view(), name='links'),
-]
